b7b214c8de3d3327c4102640524568ac9febfd8e
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-04 06:08:48 +0530

    feat: set up routing with React Router and create Home page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/students/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
 
    </div>
   )
