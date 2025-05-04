6abdaeccc9baecbd41a55edafdfec5a474e7c4ec
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-04 06:19:23 +0530

    feat: add routing for course-related pages and implement Loading component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,25 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/students/Home'
+import CourseList from './pages/students/CoursesList'
+import CourseDetails from './pages/students/CourseDetails'
+import MyEnrollments from './pages/students/MyEnrollments'
+import Player from './pages/students/Player'
+import Loading from './pages/students/Loading'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div>
 
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/course-list' element={<CourseList/>} />
+      <Route path='/course-list/:input' element={<CourseList/>} />
+      <Route path='/course/:id' element={<CourseDetails/>} />
+      <Route path='/my-enrollment' element={<MyEnrollments/>} />
+      <Route path='/player/:courseId' element={<Player/>} />
+      <Route path='/loading/:path' element={<Loading/>} />
     </Routes>
 
    </div>
