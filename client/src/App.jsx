bfaae6cd67d9b407b0c25e8df3aa832d793a1a57
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-04 06:30:27 +0530

    feat: add educator routing and components for course management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,12 @@ import CourseDetails from './pages/students/CourseDetails'
 import MyEnrollments from './pages/students/MyEnrollments'
 import Player from './pages/students/Player'
 import Loading from './pages/students/Loading'
+import Educator from './pages/educator/Educator'
+import Dashbord from './pages/educator/Dashbord'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentEnroll from './pages/educator/StudentEnroll'
+
 
 function App() {
 
@@ -22,6 +28,12 @@ function App() {
       <Route path='/my-enrollment' element={<MyEnrollments/>} />
       <Route path='/player/:courseId' element={<Player/>} />
       <Route path='/loading/:path' element={<Loading/>} />
+      <Route path='/educator' element={<Educator/>}>
+         <Route path='educator' element={<Dashbord />} />
+         <Route path='add-courses' element={<AddCourse />} />
+         <Route path='my-courses' element={<MyCourses />} />
+         <Route path='student-enrolled' element={<StudentEnroll />} />
+      </Route>
     </Routes>
 
    </div>
@@ -29,3 +41,4 @@ function App() {
 }
 
 export default App
+
