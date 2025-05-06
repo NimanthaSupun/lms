06da0b3ac0eb20fdf0cb6d821522132c6a5f4c17
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-06 06:24:17 +0530

    feat: enhance Hero and NavBar components with user authentication and conditional rendering

--- client/src/components/students/Hero.jsx
+++ client/src/components/students/Hero.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Hero = () => {
   return (
-    <div>
-        <h1>Hero</h1>
+    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
+      <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing{" "}
+        <span className="text-blue-600">elit. Ea, nam</span>
+        <img
+          src={assets.sketch}
+          alt="sketch"
+          className="md:block hidden absolute bottom-7 right-0"
+        />
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
