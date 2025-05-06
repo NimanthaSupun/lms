00711b2149d4c30a282bf8a038e1c0e7d378adc0
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-06 06:01:41 +0530

    feat: integrate Clerk user authentication in NavBar component

--- client/src/components/students/NavBar.jsx
+++ client/src/components/students/NavBar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk,UserButton, useUser} from '@clerk/clerk-react'
 
 const NavBar = () => {
 
   const isCourseListPage = location.pathname.includes('/course-list');
+
+  const {openSignIn} = useClerk()
+  const {user} = useUser()
+
+
 
   return (
     <div className= {`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-gray-500 py-4  ${isCourseListPage ? 'bg-white' :'bg-cyan-100/70'}`}>
@@ -15,7 +21,7 @@ const NavBar = () => {
           <button>Become Educator</button>
           <Link to='./my-enrollments'>My Entrollment</Link>
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
       </div>
 
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
