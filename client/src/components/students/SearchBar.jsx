ed8f1e4a882592f885fc487d5cab29f9ed0ac005
Nimantha Supun <supunnimantha199@gmail.com>
2025-05-14 23:44:53 +0530

    feat: implement SearchBar component with course search functionality

--- client/src/components/students/SearchBar.jsx
+++ client/src/components/students/SearchBar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div>
+      <form action="">
+        <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3'/>
+        <input type="text" placeholder='search for courses' className='w-full h-full' />
+        <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>search</button>
+      </form>
+    </div>
   )
 }
 
